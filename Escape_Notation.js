1d8fd212d9fd6ff68e325bfb940fe2bab17f81f8
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-05-18 09:43:21 +0600

    scape notaion

---
+++ Escape_Notation.js
@@ -0,0 +1,44 @@
+var str = "Welcome to my web page"
+console.log(str);
+
+//  '' print
+
+var str = 'Welcome to my \'web\' page'
+console.log(str);
+
+// enter new line
+
+var str = 'Welcome to my \nweb page'
+console.log(str);
+
+// tab print
+
+var str = 'Welcome to my \t web page'
+console.log(str);
+
+// \print
+
+var str = 'Welcome to my \\nweb page'
+console.log(str);
+
+// carriage return
+
+var str = 'Welcome to my \rweb page'
+console.log(str);
+
+
+// vertical tab
+
+var str = 'Welcome to my \vweb page'
+console.log(str);
+
+// backspace
+
+var str = 'Welcome to my \bweb page'
+console.log(str);
+
+// form feed
+
+var str = 'Welcome to my \fweb page'
+console.log(str);
+
